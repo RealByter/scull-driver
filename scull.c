776cf2b221526c79a3fbcd1c24068adfaed4efcc
RealByter <justavishay@gmail.com>
2024-10-13 23:09:35 +0300

    implement scull_write

--- scull.c
+++ scull.c
@@ -147,7 +147,52 @@ out:
 ssize_t scull_write(struct file *filp, const char __user *buf, size_t count, loff_t *f_pos)
 {
     printk(KERN_ALERT "scull_write\n");
-    return 0;
+
+    struct scull_dev *dev = filp->private_data;
+    struct scull_qset *dptr;
+    int quantum = dev->quantum, qset = dev->qset;
+    int itemsize = quantum * qset;
+    int item, s_pos, q_pos, rest;
+    ssize_t retval = -ENOMEM;
+
+    item = (long)*f_pos / itemsize;
+    rest = (long)*f_pos % itemsize;
+    s_pos = rest / quantum;
+    q_pos = rest % quantum;
+
+    dptr = scull_follow(dev, item);
+    if(dptr == NULL)
+        goto out;
+    if(!dptr->data)
+    {
+        dptr->data = kmalloc(qset * sizeof(char *), GFP_KERNEL);
+        if(!dptr->data)
+            goto out;
+        memset(dptr->data, 0, qset * sizeof(char *));
+    }
+    if(!dptr->data[s_pos])
+    {
+        dptr->data[s_pos] = kmalloc(quantum, GFP_KERNEL);
+        if(!dptr->data[s_pos])
+            goto out;
+    }
+
+    if(count > quantum - q_pos)
+        count = quantum - q_pos;
+
+    if(copy_from_user(dptr->data[s_pos] + q_pos, buf, count))
+    {
+        retval = -EFAULT;
+        goto out;
+    }
+    *f_pos += count;
+    retval = count;
+
+    if(dev->size < *f_pos)
+        dev->size = *f_pos;
+
+out:
+    return retval;
 }
 
 long scull_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)
