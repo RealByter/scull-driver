225e0d389e130651da191404e5c674872606f1c5
RealByter <justavishay@gmail.com>
2024-10-17 20:28:40 +0300

    implement scull_p_write and scull_p_read

--- pipe.c
+++ pipe.c
@@ -97,32 +97,99 @@ static ssize_t scull_p_read(struct file *filp, char __user *buf, size_t count, l
     while (dev->rp == dev->wp)
     {
         up(&dev->sem);
-        if(filp->f_flags & O_NONBLOCK)
+        if (filp->f_flags & O_NONBLOCK)
             return -EAGAIN;
         PDEBUG("\"%s\" reading: going to sleep\n", current->comm);
-        if(wait_event_interruptible(dev->inq, (dev->rp != dev->wp)))
+        if (wait_event_interruptible(dev->inq, (dev->rp != dev->wp)))
             return -ERESTARTSYS;
-        if(down_interruptible(&dev->sem))
+        if (down_interruptible(&dev->sem))
             return -ERESTARTSYS;
     }
 
-    if(dev->wp > dev->rp)
+    if (dev->wp > dev->rp)
         count = min(count, (size_t)(dev->wp - dev->rp));
     else
         count = min(count, (size_t)(dev->end - dev->rp));
 
-    if(copy_to_user(buf, dev->rp, count))
+    if (copy_to_user(buf, dev->rp, count))
     {
         up(&dev->sem);
         return -EFAULT;
     }
 
     dev->rp += count;
-    if(dev->rp == dev->end)
+    if (dev->rp == dev->end)
         dev->rp = dev->buffer;
     up(&dev->sem);
 
     wake_up_interruptible(&dev->outq);
     PDEBUG("\"%s\" did read %li bytes\n", current->comm, (long)count);
+    return count;
+}
+
+static int spacefree(struct scull_pipe *dev)
+{
+    if (dev->rp == dev->wp)
+        return dev->buffersize - 1;
+    return ((dev->rp + dev->buffersize - dev->wp) % dev->buffersize) - 1;
+}
+
+static int scull_getwritespace(struct scull_pipe *dev, struct file *filp)
+{
+    while (spacefree(dev) == 0)
+    {
+        DEFINE_WAIT(wait);
+
+        up(&dev->sem);
+        if (filp->f_flags & O_NONBLOCK)
+            return -EAGAIN;
+        PDEBUG("\"%s\" writing: going to sleep\n", current->comm);
+        prepare_to_wait(&dev->outq, &wait, TASK_INTERRUPTIBLE);
+        if (spacefree(dev) == 0)
+            schedule();
+        finish_wait(&dev->outq, &wait);
+        if (signal_pending(current))
+            return -ERESTARTSYS;
+        if (down_interruptible(&dev->sem))
+            return -ERESTARTSYS;
+    }
+    return 0;
+}
+
+static ssize_t scull_p_write(struct file *filp, const char __user *buf, size_t count, loff_t *f_pos)
+{
+    struct scull_pipe *dev = filp->private_data;
+    int result;
+
+    if (down_interruptible(&dev->sem))
+        return -ERESTARTSYS;
+
+    result = scull_getwritespace(dev, filp);
+    if(result)
+        return result;
+
+    count = min(count, (size_t)spacefree(dev));
+    if(dev->wp >= dev->rp)
+        count = min(count, (size_t)(dev->end - dev->wp));
+    else
+        count = min(count, (size_t)(dev->rp - dev->wp - 1));
+    
+    PDEBUG("Going to accept %li bytes to %p from %p\n", (long)count, dev->wp, buf);
+    if(copy_from_user(dev->wp, buf, count))
+    {
+        up(&dev->sem);
+        return -EFAULT;
+    }
+
+    dev->wp += count;
+    if(dev->wp == dev->end)
+        dev->wp = dev->buffer;
+    up(&dev->sem);
+
+    wake_up_interruptible(&dev->inq);
+
+    if(dev->async_queue)
+        kill_fasync(&dev->async_queue, SIGIO, POLL_IN);
+    PDEBUG("\"%s\" did write %li bytes\n", current->comm, (long)count);
     return count;
 }
